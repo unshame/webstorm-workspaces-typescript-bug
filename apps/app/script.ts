05d0ff3b55ec08c229f4ab874c10a46194361a42
Oleg Shibaev <Oleg.Shibaev@noveogroup.com>
2022-01-29 13:48:08 +0300

    --amend

--- apps/app/script.ts
+++ apps/app/script.ts
@@ -1,5 +1,5 @@
 // Run `npm i` in the root folder first.
 // Then start typing `axi` and press tab to verify that auto-import doesn't work
-// Then, move "axios": "0.21.1" from apps/app/package.json to root package.json,
-// run npm i in the root folder and try again.
-// This time the auto-import should work
+// Then, move "axios": "0.21.1" from apps/app/package.json to root package.json dependencies.
+// Run `npm i` in the root folder and try again.
+// This time the auto-import should work.
